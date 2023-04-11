1399c83c7d3359b1b59218bb7b43315e98f227a2
jrasb <okrypczyk@protonmail.com>
2023-04-12 01:23:34 +0200

    added base structure of the website, functionality remains to be added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('le silly');
+console.log("main.js loaded");
+
+const root = document.querySelector('#root-div');
+const rootChildren = root.childNodes;
+
+// about me
+const aboutme = document.querySelector(`#${rootChildren.item(3).id}`);
+const aboutmeChildren = aboutme.childNodes;
+
+// contact
+const contact = document.querySelector(`#${rootChildren.item(5).id}`);
+const contactChildren = contact.childNodes;
+
+// links
+const links = document.querySelector(`#${rootChildren.item(7).id}`)
+const linksChildren = links.childNodes;
+
