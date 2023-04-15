9fc4b7e0040c800a0ba4cd2a6cd6cb6c89383cd2
jrasb <okrypczyk@protonmail.com>
2023-04-15 23:21:33 +0200

    finalised desired functionality of page

--- js/main.js
+++ js/main.js
@@ -15,3 +15,43 @@ const contactChildren = contact.childNodes;
 const links = document.querySelector(`#${rootChildren.item(7).id}`)
 const linksChildren = links.childNodes;
 
+rootChildren.item(1).addEventListener('click', () => {
+    aboutme.classList.toggle('d-none');
+    contact.classList.toggle('d-none');
+    links.classList.toggle('d-none');
+});
+
+aboutmeChildren.item(1).addEventListener('click', () => {
+    aboutmeChildren.item(3).classList.toggle('d-none');
+});
+
+contactChildren.item(1).addEventListener('click', () => {
+    contactChildren.item(3).classList.toggle('d-none');
+});
+
+linksChildren.item(1).addEventListener('click', () => {
+    linksChildren.item(3).classList.toggle('d-none');
+});
+
+// Disables the display on load of the page
+aboutme.classList.add('d-none');
+contact.classList.add('d-none');
+links.classList.add('d-none');
+
+aboutmeChildren.forEach((e,i) => {
+    if (i == 3) {
+        e.classList.add('d-none');
+    }
+});
+
+contactChildren.forEach((e,i) => {
+    if (i == 3) {
+        e.classList.add('d-none');
+    }
+});
+
+linksChildren.forEach((e,i) => {
+    if (i == 3) {
+        e.classList.add('d-none');
+    }
+});
